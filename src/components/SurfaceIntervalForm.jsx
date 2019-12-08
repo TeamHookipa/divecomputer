a0b99643144351fe9521e0b621ad3b95c8eed248
gcalica <gcalica@hawaii.edu>
2019-12-07 22:07:34 -1000

    * Finished Minimum Surface Interval Logic (Closes #8)

--- src/components/SurfaceIntervalForm.jsx
+++ src/components/SurfaceIntervalForm.jsx
@@ -5,16 +5,17 @@ import { Form } from 'semantic-ui-react';
 class SurfaceIntervalForm extends React.Component {
   render() {
     const { index, dives, handleIntervalChange, handleSubmit } = this.props;
-
+    const isDisabled = dives[index + 1] === undefined || dives[index + 1]['INPUTSET'] === undefined;
     return (
         <React.Fragment>
           <Form onSubmit={(event, data) => handleSubmit(event, index)}>
             <Form.Input
                 placeholder={`Surface Interval (minutes)`}
                 required={true}
+                disabled={isDisabled}
                 onChange={(event, value) => handleIntervalChange(event, value, index)}
             />
-            <Form.Button color={`black`} content={`Set Surface Interval`}/>
+            <Form.Button disabled={isDisabled} color={`black`} content={`Set Surface Interval`}/>
           </Form>
         </React.Fragment>
     );
